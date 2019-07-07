ea0cd8ca2b78d9093088d32ed618340911519c63
Anastasiia Petrych <a.petrych@gmail.com>
2019-07-07 20:25:04 +0200

    Revert "Revert "#4 Minor refactoring for readability" This reverts commit b44dc12b"
    This reverts commit 15f365e0

---
+++ src/test/java/com/petrych/service/SaveScreenshotIT.java
@@ -0,0 +1,28 @@
+package com.petrych.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class SaveScreenshotIT {
+
+    // Directory with "production" screenshots
+    // TODO - instead use a test directory for tests and rewrite tests which use Util.saveScreenshot(..)
+    private static final String STORAGE_DIR = Util.getStorageDir();
+
+    private static final String URL_REAL = "https://www.apple.com/";
+
+    @Test
+    public void saveScreenshot() {
+        String fileName = Util.saveScreenshot(URL_REAL);
+
+        assertFalse(fileName.isEmpty(), "File name of a created screenshot is empty.");
+        assertTrue(FileUtil.fileExists(STORAGE_DIR, fileName), "Screenshot with the given name does not exist.");
+
+        // Clean up after the test
+        FileUtil.removeFile(STORAGE_DIR, fileName);
+        assertFalse(FileUtil.fileExists(STORAGE_DIR, fileName), "Could not remove the screenshot from the folder.");
+    }
+}
