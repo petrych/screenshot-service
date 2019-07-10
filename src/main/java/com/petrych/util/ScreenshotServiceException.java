03c1a1fd77adf8eaf5b167b461855fc34d7b6110
anastasiia <a.petrych@gmail.com>
2019-07-10 17:22:44 +0200

    Added custom exception - ScreenshotServiceException

---
+++ src/main/java/com/petrych/util/ScreenshotServiceException.java
@@ -0,0 +1,9 @@
+package com.petrych.util;
+
+public class ScreenshotServiceException extends Exception
+{
+  public ScreenshotServiceException(String message, Exception e)
+  {
+    super(message, e);
+  }
+}
