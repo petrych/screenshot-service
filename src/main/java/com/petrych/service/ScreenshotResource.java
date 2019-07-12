b95113496355ef280368d6e0e1bdf763cd9f7165
anastasiia <a.petrych@gmail.com>
2019-07-12 17:19:24 +0200

    Fixes #3. Updated response codes and error handling for getting a screenshot file by id.

--- src/main/java/com/petrych/service/ScreenshotResource.java
+++ src/main/java/com/petrych/service/ScreenshotResource.java
@@ -1,13 +1,17 @@
 package com.petrych.service;
 
 import com.petrych.db.ScreenshotDao;
+import com.petrych.util.FileUtil;
 import com.petrych.util.Util;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriInfo;
 import java.io.File;
 
@@ -20,7 +24,10 @@ public class ScreenshotResource {
     Request request;
 
     String id;
-
+    
+    private static final Logger logger = LogManager.getLogger(ScreenshotResource.class);
+    
+    
     public ScreenshotResource(UriInfo uriInfo, Request request, String id) {
         this.uriInfo = uriInfo;
         this.request = request;
@@ -31,14 +38,30 @@ public class ScreenshotResource {
     @GET
     @Produces("image/png")
     public Response getScreenshot() {
-        Screenshot screenshot = ScreenshotDao.instance.getModel().get(id);
-        if (screenshot == null)
-            throw new RuntimeException("Get: Screenshot with id " + id +  " not found");
+        Screenshot screenshot;
+        String fileName;
+        Response r;
+    
+        try {
+            screenshot = ScreenshotDao.instance.getModel().get(id);
+            fileName = screenshot.getName();
+        }
+        catch (NullPointerException e) {
+            logger.debug("Id {} does not exist.", String.valueOf(id));
+            return Response.status(Status.NOT_FOUND).build();
+        }
         
-        String fileName = screenshot.getName();
-        File file = new File(Util.getStorageDir() + File.separatorChar + fileName);
-        return Response.ok(file, "image/png").header("Screenshot ", "filename=\"" + file.getName() + "\"")
-                .build();
+        boolean fileExists = FileUtil.fileExists(Util.getStorageDir(), fileName);
+        
+        if (fileExists) {
+            File file = new File(Util.getStorageDir() + File.separatorChar + fileName);
+            r = Response.ok(file, "image/png").build();
+        }
+        else {
+            r = Response.status(Status.NOT_FOUND).build();
+        }
+        
+        return r;
     }
     
 }
