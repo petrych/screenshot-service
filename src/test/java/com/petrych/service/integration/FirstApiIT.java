2065cd2f217e7c65825277fa0f279725426fa791
Anastasiia Petrych <a.petrych@gmail.com>
2019-08-03 12:49:44 +0200

    #4 Added new tests for getting screenshots. Minor refactoring in the same test class

--- src/test/java/com/petrych/service/integration/FirstApiIT.java
+++ src/test/java/com/petrych/service/integration/FirstApiIT.java
@@ -8,6 +8,8 @@ import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.petrych.service.TestingEndpoints.LOCALHOST_ENDPOINT;
+import static com.petrych.service.TestingEndpoints.SCREENSHOTS_ENDPOINT;
 import static io.restassured.RestAssured.given;
 
 public class FirstApiIT {
@@ -18,18 +20,50 @@ public class FirstApiIT {
     public static void setup() {
         spec = new RequestSpecBuilder()
                 .setContentType(ContentType.JSON)
-                .setBaseUri("http://localhost:8080/")
+                .setBaseUri(LOCALHOST_ENDPOINT.toString())
                 .addFilter(new ResponseLoggingFilter())
                 .addFilter(new RequestLoggingFilter())
                 .build();
     }
 
     @Test
-    public void getAllScreenshots() {
+    public void getAllScreenshots_DB_Access_Ok() {
         given(spec)
                 .when()
-                .get("screenshots")
+                .get(SCREENSHOTS_ENDPOINT.toString())
                 .then()
                 .statusCode(200);
     }
+
+
+    @Test
+    public void getScreenshot_Image_Exists() {
+        given(spec).pathParam("id", 1)
+                .when()
+                .get(SCREENSHOTS_ENDPOINT.toString() + "/{id}")
+                .then()
+                .contentType("image/png")
+                .statusCode(200);
+    }
+
+
+    @Test
+    public void getScreenshot_Image_Not_Exists() {
+        given(spec).pathParam("id", 3)
+                .when()
+                .get(SCREENSHOTS_ENDPOINT.toString() + "/{id}")
+                .then()
+                .statusCode(404);
+    }
+
+
+    @Test
+    public void getScreenshot_Id_Not_Exists() {
+        given(spec).pathParam("id", "k")
+                .when()
+                .get(SCREENSHOTS_ENDPOINT.toString() + "/{id}")
+                .then()
+                .statusCode(404);
+    }
+
 }
